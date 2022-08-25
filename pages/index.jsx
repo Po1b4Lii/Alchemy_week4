e16824882c92fbdff5cfe33587479f9094512d24
pDFLrmLN <tbdh1218@gmail.com>
2022-08-25 13:47:41 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,7 +42,8 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+      // const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+      const api_key = "AN3Wr_trTycdea0TxVGLoepH9VVtrZBg"
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
