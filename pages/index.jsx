faede0884af2e240c6ae6d6479411693a23764b3
pDFLrmLN <tbdh1218@gmail.com>
2022-08-25 13:14:21 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,9 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    // const api_key = "67cOOUROEGg7FApnc_U-ihL43EZGqYsl"
+    // const api_key = ""
+    const api_key = "AN3Wr_trTycdea0TxVGLoepH9VVtrZBg"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
